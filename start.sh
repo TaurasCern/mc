b89a94ad45a3875a0cb778f3bb869e2954411b0c
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2024-07-02 20:20:19 +0300

    increased sleep timer

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 screen -dmS minecraft java -Xmx6G -Xms6G -jar /minecraft/fabric-server-launch.jar nogui
 
 # wait for the server to start, otherwise container logs will be empty
-sleep 5
+sleep 10
 
 tail -f /minecraft/logs/latest.log
