5ba4dcf2c7b2fefc57427cebc949d26ef8ea17c0
Tauras Č <51359787+TaurasCern@users.noreply.github.com>
2024-07-12 21:05:53 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-screen -dmS minecraft java -Xmx6G -Xms6G -jar /minecraft/fabric-server-launch.jar nogui
+screen -dmS minecraft java -Xmx6G -Xms6G -jar /minecraft/fabric-server-launch.jar nogui --safeMode
 
 # wait for the server to start, otherwise container logs will be empty
 sleep 10
